35639f948d05a41085d6e33b7ea179e57f3274df
Nancy Jackson <nbjackson23@gmail.com>
2020-08-08 17:35:49 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -25,8 +25,8 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
   shipping_manifest["muskets"] = 2
-shipping_manifest["gun powder"] = 4
-return shipping_manifest
+  shipping_manifest["gun powder"] = 4
+  return shipping_manifest
 end
 
 
