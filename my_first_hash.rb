a3db61542b378f06c50acbbf346bfa6385f8bc33
Nancy Jackson <nbjackson23@gmail.com>
2020-08-08 17:17:56 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -9,4 +9,16 @@ the_manifest = {"whale bone corsets" => 5,
                 "oil paintings" => 3}
 end
 
-def 
+def retrieval
+  shipping_manifest = {
+                  "whale bone corsets" => 5, 
+                  "porcelain vases" => 2, 
+                  "oil paintings" => 3
+                 }
+  hash = {"oil paintings" => 3}
+  hash[2] = 3
+
+  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+
+end
+
